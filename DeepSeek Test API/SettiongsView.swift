e29c06c4c44d82b6527b34753a74a8cc73c1ba63
Ilyas <myjobil.ku@gmail.com>
2025-09-15 12:14:43 +0300

    [Feature] add dosomething state

---
+++ DeepSeek Test API/SettiongsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettiongsView.swift
+//  DeepSeek Test API
+//
+//  Created by Ильяс on 15.09.2025.
+//
+
+import SwiftUI
+
+struct SettiongsView: View {
+    
+    @State private var doSomething: Bool = false
+    
+    var body: some View {
+        Text("Settings View!")
+    }
+}
+
+//#Preview {
+//    SettiongsView()
+//}
